4c1a40335350aca37f5aa23c3e1262ff85f398a0
celtic93 <egorka80@list.ru>
2019-10-16 00:08:36 +0300

    app.rb added

--- app.rb
+++ app.rb
@@ -1,3 +1,25 @@
 class App
+  
+  def call(env)
+    @env = env
+    [status, headers, body]
+  end
 
+  private
+
+  def status
+    if @env['REQUEST_PATH'] == '/time'
+      200
+    else
+      404
+    end
+  end
+
+  def headers
+    {'Content-type' => 'text/plain'}
+  end
+
+  def body
+    []
+  end
 end
